c81a13ad80acef82ed0fda8859e030303ed21be6
vicuartas230 <vicuartas230@gmail.com>
2021-09-27 20:52:33 -0500

    Upload last changes

--- interpol.js
+++ interpol.js
@@ -1,37 +1,22 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     $.ajax({
-//         type: "POST",
-//         url: "https://nufi.azure-api.net/ficharoja/v2/consultar",
-//         headers: {
-//             'NUFI-API-KEY': '40fb302751d74a6d8122afe2b5cb8b1a'
-//         },
-//         data: JSON.stringify({
-//             'nombre': 'Diana',
-//             'apellido': 'Parra Sierra',
-//             'sexo': 'F'
-//         }),
-//         contentType: "application/json",
-//         dataType: "json",
-//         success: function (response) {
-//             console.log(response);
-//         }
-//     });
-// });
-
-$.ajax({
-    type: "POST",
-    url: "https://nufi.azure-api.net/ficharoja/v2/consultar",
-    headers: {
-        'NUFI-API-KEY': '40fb302751d74a6d8122afe2b5cb8b1a'
-    },
-    data: JSON.stringify({
-        'nombre': 'Diana',
-        'apellido': 'Parra Sierra',
-        'sexo': 'F'
-    }),
-    contentType: "application/json",
-    dataType: "json",
-    success: function (response) {
-        console.log(response);
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    let name, lname, genre;
+    $('#search').click(function (e) { 
+        $.ajax({
+            type: "POST",
+            url: "https://nufi.azure-api.net/ficharoja/v2/consultar",
+            headers: {
+                'NUFI-API-KEY': '40fb302751d74a6d8122afe2b5cb8b1a'
+            },
+            data: JSON.stringify({
+                "nombre": $('#nombre').val(),
+                "apellidos": $('#apellidos').val(),
+                "sexo": $('#sexo').val()
+            }),
+            contentType: "application/json",
+            dataType: "json",
+            success: function (response) {
+                console.log(response);
+            }
+        });
+    });
 });
