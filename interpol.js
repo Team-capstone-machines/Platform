211c3e128c6e5133e073c466dce7a7479f4d35c5
dianaparr <dianaparra.9611@gmail.com>
2021-09-28 10:10:05 -0500

    New actualization with 3 ways

--- interpol.js
+++ interpol.js
@@ -1,26 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
 $("#search").click(function (e) {
-    $.ajax({
+    const settings = {
         async: true,
         crossDomain: true,
-        type: "POST",
         url: "https://nufi.azure-api.net/ficharoja/v2/consultar",
+        method: "POST",
         headers: {
-            'NUFI-API-KEY': '40fb302751d74a6d8122afe2b5cb8b1a',
-            'Access-Control-Allow-Origin': '*'
+            "Content-Type": "application/json",
+            "NUFI-API-KEY": "40fb302751d74a6d8122afe2b5cb8b1a",
         },
-        data: JSON.stringify({
-            "nombre": $('#nombre').val(),
-            "apellidos": $('#apellidos').val(),
-            "sexo": $('#sexo').val()
-        }),
-        contentType: "application/json",
-        dataType: "json",
-        success: function (data) {
-            $('.response').text(data);
-        }
+        processData: false,
+        data: "{\"nombre\":\"TATIANA\", \"apellidos\": \"IVANENKO\", \"sexo\": \"\"}",
+        };
+
+        $.ajax(settings).done(function (response) {
+        console.log(response.json());
+        });
     });
-});
 });
 
 // $('#search').click(function (e) {
@@ -41,4 +37,24 @@ $("#search").click(function (e) {
 //       console.log(response);
 //     });
 //     });
+// });
+
+// $.ajax({
+//     async: true,
+//     crossDomain: true,
+//     type: "POST",
+//     url: "https://nufi.azure-api.net/enriquecimientoinformacion/v1/busqueda",
+//     headers: {
+//         'Ocp-Apim-Subscription-Key': '40fb302751d74a6d8122afe2b5cb8b1a',
+//         //'strict-origin-when-cross-origin': 'http://127.0.0.1:5500/index.html'
+//         'Access-Control-Allow-Origin': 'http://127.0.0.1:5500/index.html'
+//     },
+//     data: JSON.stringify({
+//         "telefono": $('#nombre').val(),
+//     }),
+//     contentType: "application/json",
+//     dataType: "json",
+//     success: function (data) {
+//         $('.response').text(data);
+//     }
 // });
